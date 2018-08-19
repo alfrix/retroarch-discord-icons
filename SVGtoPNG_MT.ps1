41a186b391467529165dfb07f5ddf861a37f3a4c
Alfredo Monclus <alfredomonclus@gmail.com>
2018-08-19 05:44:51 -0300

    Initial commit

---
+++ SVGtoPNG_MT.ps1
@@ -0,0 +1,32 @@
+# Alfrix 2017
+# 
+# Batch Convert SVG to PNG
+[int]$jobs=4
+[string]$input_folder="$PSScriptRoot\SVG"
+[string]$output_folder="$PSScriptRoot\PNG"
+$StartTime = $(get-date)
+echo ""
+Write-Host "Converting SVG to PNG" -ForegroundColor Green
+echo ""
+get-date -format T
+echo ""
+for ($i=0; $i -lt $jobs; $i++){
+	$args = @($input_folder,$output_folder,$jobs,$i) 
+	Start-Job -FilePath .\SVGtoPNG_MT_Worker.ps1 -ArgumentList $args
+}
+
+ 
+Get-Job | Wait-Job
+
+$elapsedTime = $(get-date) - $StartTime
+$totalTime = "{0:HH:mm:ss}" -f ([datetime]$elapsedTime.Ticks)
+echo ""
+get-date -format T
+echo ""
+echo $totalTime
+echo ""
+echo "Show results?"
+pause
+
+Get-Job | Receive-Job | Out-GridView
+pause
---
+++ SVGtoPNG_MT_Worker.ps1
@@ -0,0 +1,13 @@
+# Alfrix 2017
+#
+# Batch Convert SVG to PNG
+param([string]$svg_path, [string]$png_path, [int]$job_total, [int]$this_job)
+# Use Inkscape 0.48 (before cairo)
+d:
+cd "d:\Inkscape"
+$svgs = Get-ChildItem $svg_path\* -Include *.svg
+for ($i=$this_job; $i -lt $svgs.Count; $i=$i+$job_total) {
+	$svg_fullname=$svgs[$i].fullname
+	$svg_name=$svgs[$i].basename
+	.\inkscape.com -f "$svg_fullname"  -w 1024 -h 1024 -z -e "$png_path\$svg_name.png"
+}
